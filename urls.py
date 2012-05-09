245c4525434d5f2300a4d0cae58d8382073cae18
Matthew Gallagher <mattva01@gmail.com>
2012-05-08 23:48:00 -0400

    Added password change and password reset tools

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.views.generic import CreateView, ListView, DetailView, RedirectView
 from wordviewer.models import WordEntry
 from wordviewer.views import register, WordEntryCreationView, WordEntryUpdateView
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout, password_change, password_change_done, password_reset, password_reset_done, password_reset_confirm
 # Uncomment the next two lines to enable the admin:
 
 from django.contrib import admin
@@ -17,6 +17,11 @@ urlpatterns = patterns('',
     url(r'^words/$', ListView.as_view(model=WordEntry,queryset=WordEntry.objects.order_by('name'))),
     url(r'^words/(?P<pk>\d+)/$', DetailView.as_view(model=WordEntry)), 
     url(r'^register/$',register),
+    url(r'^password_change/$',password_change),
+    url(r'^password_change/done/$',password_change_done),
+    url(r'^password_reset/$',password_reset),
+    url(r'^password_reset/done/$',password_reset_done),
+    url(r'^password_reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',password_reset_confirm),
     url(r'^login/$',login),
     url(r'^logout/$',logout,{'next_page': '/'}),
     # Uncomment the admin/doc line below to enable admin documentation:
